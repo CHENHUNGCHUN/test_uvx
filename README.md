11e7ff95b24e6d1cf093e80e27f7655fdf602dce
kovaca <kojunjunla@gmail.com>
2025-04-10 08:47:16 +0800

    change package to MCP

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 # hello-how-are-you
 
 This is a simple demo for uvx. It prints "how are you".
--- hello/__main__.py
+++ hello/__main__.py
@@ -1,5 +1,16 @@
-def main():
-    print("how are you")
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
 
+# Initialize FastMCP server
+mcp = FastMCP("hello")
+
+@mcp.tool()
+def main():
+    return "hello how are you"
+
+# if __name__ == "__main__":
+#     main()
 if __name__ == "__main__":
-    main()
+    # Initialize and run the server
+    mcp.run(transport='stdio')
